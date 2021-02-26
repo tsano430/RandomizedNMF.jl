d7372c29066bb102c38ab7517843422cc450c557
Takehiro Sano <tsano430@gmail.com>
2021-02-26 15:17:38 +0900

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,7 +11,11 @@ X = rand(100, 50)
 k = 5
 maxiter = 10
 W, H = rnmf(X, k, maxiter=10)
+@test all(W .>= 0.0)
+@test all(H .>= 0.0)
 
 # Test 2
 Y = rand(50, 100)
 W, H = rnmf(Y, k, maxiter=10)
+@test all(W .>= 0.0)
+@test all(H .>= 0.0)
